9b5542ab4369553c7f1ec810f6055bba023a1631
Kazuya Kawamoto <kazuya_kawamoto@fronteo.com>
2018-06-08 15:02:18 +0900

    第6回勉強会資料

---
+++ report.py
@@ -0,0 +1,7 @@
+def get_current_weather():
+	from random import choice
+	weather = ['rain', 'sun', 'sleet', 'fog', 'snow']
+	return choice(weather)
+
+def print_num():
+	print("1")
---
+++ zoo.py
@@ -0,0 +1,2 @@
+def hours():
+	print("daily")
